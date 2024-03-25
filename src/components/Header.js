447ad91e25f35fce6ef3ee56ae4e68d3857dd3d9
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-03-26 01:43:51 +0530

    all chages

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,53 +7,57 @@ import { useSelector } from "react-redux";
 import { removeItem } from "../utils/storage";
 
 const Header = (props) => {
-  const user = useSelector((state) => state.user);
+   const user = useSelector((state) => state.user);
 
-  let history = useNavigate();
-  const { setIsLobby, isLobby } = props;
+   let history = useNavigate();
+   const { setIsLobby, isLobby } = props;
 
-  //To exit game //
-  const exitGame = () => {
-    //removing user from local storage //
-    removeItem("user");
-    setIsLobby(true);
-    //redirecting in lobby
-    history("/");
-  };
+   //To exit game //
+   const exitGame = () => {
+      //removing user from local storage //
+      removeItem("user");
+      setIsLobby(true);
+      //redirecting in lobby
+      history("/");
+   };
 
-  return (
-    <section className="flex relative z-10 text-white items-center justify-between">
-      {!isLobby ? (
-        <div className="flex items-center">
-          <img alt="" className="w-20" src={gameLogo} />
-          <p className="text-xl font-bold">Exploding Kitten </p>
-        </div>
-      ) : (
-        <div></div>
-      )}
-      {!isLobby && user?.username && (
-        <div className="flex space-x-2  pl-6">
-          <div className="flex bg-white px-4 py-1 text-xl rounded-md space-x-3">
-            <p className=" text-black font-bold">NAME : </p>
-            <p className="font-bold  text-[rgb(132,204,22)]">{user.username}</p>
-          </div>
-          <div className="flex bg-white px-4 py-1 text-xl rounded-md space-x-3">
-            <p className=" text-black  font-bold">POINTS :</p>
-            <p className="font-bold   text-[rgb(132,204,22)]">{user.matchesWon}</p>
-          </div>
-        </div>
-      )}
-      <div className="flex text-xl px-14 py-4 space-x-8 items-center">
-        {/*<Rules /> */}
-        {!isLobby && <Leaderboard />}
-        {!isLobby && (
-          <p onClick={exitGame} className="cursor-pointer font-bold">
-            Exit
-          </p>
-        )}
-      </div>
-    </section>
-  );
+   return (
+      <section className="flex relative z-10 text-white items-center justify-between">
+         {!isLobby ? (
+            <div className="flex items-center">
+               <img alt="" className="w-20" src={gameLogo} />
+               <p className="text-xl font-bold text-white">Exploding Kitten </p>
+            </div>
+         ) : (
+            <div></div>
+         )}
+         {!isLobby && user?.username && (
+            <div className="flex space-x-2  pl-6">
+               <div className="flex bg-white px-4 py-1 text-xl rounded-md space-x-3">
+                  <p className=" text-black font-bold">NAME : </p>
+                  <p className="font-bold  text-[rgb(132,204,22)]">
+                     {user.username}
+                  </p>
+               </div>
+               <div className="flex bg-white px-4 py-1 text-xl rounded-md space-x-3">
+                  <p className=" text-black  font-bold">POINTS :</p>
+                  <p className="font-bold   text-[rgb(132,204,22)]">
+                     {user.matchesWon}
+                  </p>
+               </div>
+            </div>
+         )}
+         <div className="flex text-xl px-14 py-4 space-x-8 items-center">
+            {/*<Rules /> */}
+            {!isLobby && <Leaderboard />}
+            {!isLobby && (
+               <p onClick={exitGame} className="cursor-pointer font-bold">
+                  Exit
+               </p>
+            )}
+         </div>
+      </section>
+   );
 };
 
 export default Header;
--- src/pages/GameLobby.js
+++ src/pages/GameLobby.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import bgImage from "../assets/img/bg-picture.jpg";
-import gameLogo from "../assets/img/gameLogo.png";
+//import bgImage from "../assets/img/bg-picture.jpg";
+//import gameLogo from "../assets/img/gameLogo.png";
 import { useToast } from "@chakra-ui/react";
 import { request } from "../api/request";
 import { setItem } from "../utils/storage";
@@ -33,37 +33,26 @@ const GameLobby = (props) => {
       }
    };
 
-   const togglePreLoader = () => {
-      let elem = document.getElementById("preloader");
-      elem.style.display = "none";
-   };
-
    return (
-      <main className="flex w-auto items-center h-[87vh] justify-center align-middle">
+      <main className="flex w-full items-center  h-[87vh] justify-center ">
          <div
-            id="preloader"
-            className="w-full flex flex-col justify-center  items-center h-[100vh] absolute top-0 z-20">
-            <p className="text-2xl font-bold text-white">Exploding Kitten </p>
-         </div>
-
-         <div
-            className="z-10  flex flex-col border-2 border-[black]  justify-center
-         space-y-8 mr-60 rounded-lg px-6 h-72 
+            className="z-10  flex flex-col border-2 border-l-4 border-b-4 border-[black] bg-black justify-center
+         space-y-8 rounded-lg px-6 h-72 
          ">
-            <p className=" text-2xl -mb-4  font-bold text-black">
+            <p className=" text-2xl -mb-4  font-bold text-white">
                ENTER USERNAME
             </p>
             <input
                type="text"
                placeholder=""
-               className="w-80 px-4 py-2  outline-none rounded-lg border border-black"
+               className="w-80 px-4 py-2  outline-none rounded-lg"
                onChange={(e) => setUsername(e.target.value)}
             />
             <input
                type="button"
                value="START GAME"
                id="button"
-               className="bg-orange-600 rounded-lg w-full py-2 cursor-pointer border border-black hover:bg-orange-700"
+               className="bg-white rounded-lg w-full py-2 border cursor-pointer border-black "
                onClick={() => startGame()}
             />
          </div>
